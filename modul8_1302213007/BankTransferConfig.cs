05b8efc5009ee8893051ea98907695018ca83886
ahmdmuflih <ahmdmuflih@student.telkomuniversity.ac.id>
2023-04-10 08:28:02 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302213007/BankTransferConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+class BankTransferConfig
+{
+	public string Lang { get; set; }
+	public TransferConfig Transfer { get; set; }
+	public List<string> Methods { get; set; }
+	public ConfirmationConfig Confirmation { get; set; }
+
+	public class TransferConfig
+	{
+		public int Threshold { get; set; }
+		public int LowFee { get; set; }
+		public int HighFee { get; set; }
+	}
+
+	public class ConfirmationConfig
+	{
+		public string En { get; set; }
+		public string Id { get; set; }
+	}
+
+	public static BankTransferConfig Load(string filePath)
+	{
+		if (!File.Exists(filePath))
+		{
+			return new BankTransferConfig
+			{
+				Lang = "en",
+				Transfer = new TransferConfig
+				{
+					Threshold = 25000000,
+					LowFee = 6500,
+					HighFee = 15000
+				},
+				Methods = new List<string>
+				{
+					"RTO (real time)",
+					"SKN",
+					"RTGS",
+					"BI FAST"
+				},
+				Confirmation = new ConfirmationConfig
+				{
+					En = "yes",
+					Id = "ya"
+				}
+			};
+		}
+
+		string json = File.ReadAllText(filePath);
+		return JsonConvert.DeserializeObject<BankTransferConfig>(json);
+	}
+}
--- modul8_1302213007/Program.cs
+++ modul8_1302213007/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Reflection.Metadata;
+
+class Program
+{
+    static void Main()
+    {
+        BankTransferConfig config = new BankTransferConfig.Load("D:\Dokumen\Kuliah\Semester 4\Konstruksi Perangkat Lunak\modul8_1302213007\modul8_1302213007\bank_transfer_config.json");
+
+        Console.WriteLine(config.Lang == "en" ? "Please insert the amount of money to transfer : " : "Masukkan jumlah uang yang akan di transfer : ");
+        int amount = int.Parse(Console.ReadLine());
+
+        int transferFee = amount <= config.Transfer.Threshold ? config.Transfer.HighFee : config.Transfer.LowFee;
+        int totalAmount = amount + transferFee;
+
+        Console.WriteLine(config.Lang == "en" ? $"Transfer fee = {transferFee}" : $"Biaya transfer = {transferFee}");
+        Console.WriteLine(config.Lang == "en" ? $"Total amount = {totalAmount}" : $"Total biaya = {totalAmount}");
+
+        Console.WriteLine(config.Lang == "en" ? "Select Transfer Method : " : "Pilih metode transfer : ");
+        for (int i = 0; i < config.Methods.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {config.Methods[i]}");
+        }
+
+        Console.Write(config.Lang == "en" ? $"Please type \"{config.Confirmation.En}\" to confirm the transaction : " : $"Ketik \"{config.Confirmation.Id}\" untuk mengkonfimasi transaksi : ");
+        string confirmation = Console.ReadLine();
+
+        if (confirmation == config.Confirmation.En || confirmation == config.Confirmation.Id)
+        {
+            Console.WriteLine(config.Lang == "en" ? "The transfer is complet " : "Proses transaksi berhasil");
+        } else
+        {
+            Console.WriteLine(config.Lang == "en" ? "Transfer is cancelled" : "Transfer dibatalkan");
+        }
+    }
+}
